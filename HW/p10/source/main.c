aa62b5f546647413239face22ae8da3884b25fc1
108360206 <55587318+108360206@users.noreply.github.com>
2019-11-28 22:13:49 +0800

    Add files via upload

---
+++ HW/p10/source/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cubeByvalue(int *nPtr);
+
+int main(void)
+{
+	int number = 5;
+	printf("The original value of number is %d", number);
+
+	cubeByvalue(&number);
+
+	printf("\n new The value of number is %d\n", number);
+
+	system("pause");
+	return 0;
+}
+
+int cubeByvalue(int *nPtr)
+{
+	*nPtr = *nPtr * *nPtr * *nPtr;
+}
